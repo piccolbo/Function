272951439316e3efdda8e399e3cfef759088f26a
Antonio Piccolboni <antonio@piccolboni.info>
2015-11-04 19:22:19 -0800

    additional test features

--- pkg/R/test.R
+++ pkg/R/test.R
@@ -4,3 +4,17 @@ test.default =
   function(x, ...)
     map(attr(x, "tests", exact = TRUE), ~.())
 
+test.list =
+test.environment =
+  function(x, filter = testable, ...)
+    map(keep(as.list(x), filter), ~test(.))
+
+testable =
+  function(x)
+    !is.null(attributes(x)$tests)
+
+package.test =
+  function(x, filter = testable, ...) {
+    library(x, character.only = TRUE)
+    env = as.environment(paste0("package:", x))
+    test(env, filter = filter)}
